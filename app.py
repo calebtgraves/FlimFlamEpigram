d6bcd25af88423f0613e4f4f8d64f2fdf3bbbce9
Ben <109660600+benmahomie@users.noreply.github.com>
2023-11-21 15:02:08 -0700

    networking

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 from logic import QuiplashGame
 
 app = Flask(__name__)
@@ -16,8 +16,28 @@ def client():
 
 @socketio.on('connect')
 def handle_connect():
-    # Handle new connection
-    pass
+    print("A new player has connected.")
+    # Here you can perform actions needed when a new player connects
+    # For example, sending a welcome message
+    emit('welcome_message', {'message': 'Welcome to the game!'})
+
+connected_players = {}
+
+@socketio.on('register_player')
+def handle_player_registration(data):
+    player_name = data['name']
+    session_id = request.sid
+    print(f"Player {player_name} has joined the game with session ID {session_id}.")
+    connected_players[player_name] = session_id
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    session_id = request.sid
+    if session_id in connected_players:
+        player_name = connected_players[session_id]
+        print(f"Player {player_name} has disconnected.")
+        # Remove player from the list
+        del connected_players[session_id]
 
 # Additional SocketIO events for game logic here...
 
