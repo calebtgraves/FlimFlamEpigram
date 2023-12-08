33b09bc87f04fc9baa12ff5d5df6674394dc0b4a
Ben <109660600+benmahomie@users.noreply.github.com>
2023-12-07 23:09:40 -0700

    added crutches.txt to load_prompts

--- logic.py
+++ logic.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import emit
 import random
 import time
 
@@ -10,161 +9,7 @@ class EpigramGame:
         self.scores = {}   # Dictionary to keep track of scores
         self.prompts = []  # List of all prompts
         self.special_activities = ['acro_lash', 'comic_lash', 'word_lash']  # Special activities
-        self.crutches = [ # List of crutches (like safety quips)
-            'A beetle with an attitude', 
-            'Your mom', 
-            'Loaded Banana',
-            'Your friend Kevin',
-            'Flim-Flam Epigram',
-            "Clown shoes",
-            "Moonwalking fish",
-            "Tofu sword fights",
-            "Banana peel surfing",
-            "Giraffe ballet",
-            "Wombat rodeo",
-            "Disco chickens",
-            "Ninja librarians",
-            "Jellybean juggling",
-            "Marshmallow dodgeball",
-            "Sock puppetry",
-            "Penguin tap-dance",
-            "Llama opera",
-            "Pajama parade",
-            "Synchronized sneezing",
-            "Bagpipe karaoke",
-            "Bubblegum sculpting",
-            "Pirate pancake",
-            "Robot poetry",
-            "Pickle juggling",
-            "Boomerang Frisbee",
-            "Snail racing",
-            "Zombie gardening",
-            "Elephant limbo",
-            "Cupcake cannon",
-            "Laser tag marshmallows",
-            "Alien salsa",
-            "Lawn mower ballet",
-            "Cactus pillow",
-            "T-Rex yoga",
-            "Unicorn rodeo",
-            "Squirrel karate",
-            "Disco ninja",
-            "Fish fashion",
-            "Hula hoop marathon",
-            "Taco Tuesday",
-            "Penguin wrestling",
-            "Jedi gardening",
-            "Juggling cacti",
-            "Ninja disco",
-            "Llama rodeo",
-            "Robot tap-dance",
-            "Pancake Olympics",
-            "Kangaroo karaoke",
-            "Pirate pogo",
-            "Giraffe surfing",
-            "Wombat opera",
-            "Moonwalking llamas",
-            "Jellybean ballet",
-            "Tofu juggling",
-            "Banana hammocks",
-            "Disco sloths",
-            "Robot rodeo",
-            "Cupcake fencing",
-            "Zombie fashion",
-            "Pickle limbo",
-            "Elephant disco",
-            "Alien rodeo",
-            "Lawn mower opera",
-            "Synchronized llamas",
-            "Boomerang gardening",
-            "Snail ballet",
-            "Laser tag pillow",
-            "Unicorn pancake",
-            "Marshmallow yoga",
-            "Cactus karaoke",
-            "Fish cannon",
-            "Hula hoop ninja",
-            "Taco sculpture",
-            "Penguin Frisbee",
-            "Jedi fashion",
-            "Squirrel rodeo",
-            "Disco gardening",
-            "T-Rex marathon",
-            "Giraffe limbo",
-            "Pirate yoga",
-            "Wombat disco",
-            "Jellybean wrestling",
-            "Moonwalking cacti",
-            "Elephant tap-dance",
-            "Cupcake opera",
-            "Zombie karaoke",
-            "Robot fencing",
-            "Alien ballet",
-            "Lawn mower surfing",
-            "Pickle fashion",
-            "Banana pillow",
-            "Unicorn cannon",
-            "Juggling pancakes",
-            "Marshmallow rodeo",
-            "Disco ninja turtles",
-            "Synchronized llamas",
-            "Boomerang opera",
-            "Snail yoga",
-            "Laser tag disco",
-            "Elephant fencing",
-            "Cactus karaoke",
-            "Alien limbo",
-            "Lawn mower fashion",
-            "Hula hoop pogo",
-            "T-Rex rodeo",
-            "Giraffe karaoke",
-            "Pirate disco",
-            "Wombat ninja",
-            "Jellybean fashion",
-            "Moonwalking pancakes",
-            "Unicorn yoga",
-            "Marshmallow fencing",
-            "Pickle opera",
-            "Banana rodeo",
-            "Synchronized turtles",
-            "Disco cacti",
-            "Robot yoga",
-            "Jedi limbo",
-            "Fish disco",
-            "Hula hoop opera",
-            "Elephant pogo",
-            "Zombie fencing",
-            "Cupcake karaoke",
-            "Alien tap-dance",
-            "Laser tag pancake",
-            "Lawn mower ballet",
-            "Boomerang ninja",
-            "Snail rodeo",
-            "T-Rex opera",
-            "Giraffe disco",
-            "Pancake fencing",
-            "Pirate yoga",
-            "Banana ninja",
-            "Wombat limbo",
-            "Synchronized disco",
-            "Jellybean tap-dance",
-            "Unicorn fencing",
-            "Marshmallow ninja",
-            "Pickle yoga",
-            "Elephant rodeo",
-            "Disco opera",
-            "Robot fencing",
-            "Jedi disco",
-            "Alien ballet",
-            "Fish karaoke",
-            "Hula hoop pancake",
-            "Cupcake tap-dance",
-            "Laser tag ninja",
-            "Lawn mower yoga",
-            "Snail disco",
-            "T-Rex fencing",
-            "Giraffe pogo"
-            ]
+        self.crutches = [] # List of crutches (like safety quips)
         self.load_prompts()
 
     def run_game(self):
@@ -175,17 +20,24 @@ class EpigramGame:
         self.show_winner()
 
     def load_prompts(self, shuffle=True):
+        # Load prompts
         with open('prompts/prompts.txt', 'r') as prompts:
             for prompt in prompts:
                 self.prompts.append(prompt.strip())
         if shuffle:
             random.shuffle(self.prompts)
 
+        # Load crutches
+        with open('prompts/crutches.txt', 'r') as crutches:
+            for crutch in crutches:
+                self.crutches.append(crutch.strip())
+
     def show_instructions(self):
         # Display game instructions
+        wait_time = 15
+        print(f'Waiting {wait_time} seconds for players to read instructions...')
         emit("instructions", room=self.host_sid)
-        # TODO: This could be a good place for Yodahe to adapt player
-        # instructions using the documentation. This is a near-the-end step.
+        time.sleep(wait_time) # Wait for a bit so players can read the instructions
 
     def play_round(self, round_num):
         # Logic for each round
@@ -198,7 +50,7 @@ class EpigramGame:
         self.show_results()
         self.update_leaderboard()
 
-    def get_prompts(self,numPlayers:int):
+    def get_prompts(self, numPlayers:int):
         playerPrompts = [[] for i in range(numPlayers)]
         selectedPrompts = set() # All of the prompts to be used during this game. It's a set so that there will be no duplicates.
         for i in range(2): # For the first and second rounds. The third round will only have one prompt.
@@ -238,8 +90,8 @@ class EpigramGame:
     def deliver_prompts(self, prompt_pairs: tuple):
         prompt_pairs = prompt_pairs # Deliver 2 prompts per player
         for i in range(len(self.players)): # TODO: Finish this part and receiving part in javascript
-            emit('new_prompt', {'prompt': self.player_dict[i]['name']}, room=self.player_dict[i]['sid'])
-            emit('new_prompt', {'prompt': self.player_dict[i]['name']}, room=self.player_dict[i]['sid'])
+            emit('new_prompt', {'prompt': self.players[i]['name']}, room=self.players[i]['sid'])
+            emit('new_prompt', {'prompt': self.players[i]['name']}, room=self.players[i]['sid'])
 
     def collect_answers(self):
         # TODO: Need frontend solution to collect answers from players for prompts
