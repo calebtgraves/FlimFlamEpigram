b713e8df21db9060babfd54d6f3ce52292a2ed66
Ben <109660600+benmahomie@users.noreply.github.com>
2023-12-08 02:44:19 -0700

    hoo boy, lotsa stuff Added comic lash comics

--- logic.py
+++ logic.py
@@ -1,45 +1,50 @@
+from flask import request
 from flask_socketio import SocketIO, emit
 import random
-import time
 
 class EpigramGame:
     def __init__(self, socketio: SocketIO, players: list, host_sid):
         self.socketio = socketio
-        self.players = players  # List of player objects, including names, sids for emits, etc.
+        # List of player dicts: 
+            #[{'name': player_name, 'vip': False, 'sid': session_id, 
+            #'score': 0, 'votes': {'round1': [], 'round2': [], 'round3': []},
+            #'color':player_color}]
+        self.players = players
         self.host_sid = host_sid # Host sid (for emit(room=host_sid)
         self.scores = {}   # Dictionary to keep track of scores
         self.prompts = []  # List of all prompts
-        self.special_activities = ['acro_lash', 'comic_lash', 'word_lash']  # Special activities
+        self.prompt_answers = {'round 1': {}, 'round 2': {}, 'round 3': {}} # prompts and answers (i.e. {'round x': {'prompt x': {'player 1': {'answer': answer, 'votes': [votes_by_name]}}})
+        self.special_activities = [self.acro_lash, self.comic_lash, self.word_lash]  # Special activities
         self.crutches = [] # List of crutches (like safety quips)
+
         self.load_prompts()
-        self.socketio.on_event('answer', self.collect_answers)
+        self.socketio.on_event('answer', self.receive_answers) # Get prompt answers
+        self.socketio.on_event('vote', self.receive_votes) # Get player votes
+
+    def load_prompts(self, shuffle=True):
+            # Load prompts
+            with open('prompts/prompts.txt', 'r') as prompts:
+                for prompt in prompts:
+                    self.prompts.append(prompt.strip())
+            if shuffle:
+                random.shuffle(self.prompts)
+
+            # Load crutches
+            with open('prompts/crutches.txt', 'r') as crutches:
+                for crutch in crutches:
+                    self.crutches.append(crutch.strip())
 
     def run_game(self):
         self.show_instructions()
         for round_num in range(1, 3):
             self.play_round(round_num)
         self.play_special_round()
-        self.show_winner()
-
-    def load_prompts(self, shuffle=True):
-        # Load prompts
-        with open('prompts/prompts.txt', 'r') as prompts:
-            for prompt in prompts:
-                self.prompts.append(prompt.strip())
-        if shuffle:
-            random.shuffle(self.prompts)
-
-        # Load crutches
-        with open('prompts/crutches.txt', 'r') as crutches:
-            for crutch in crutches:
-                self.crutches.append(crutch.strip())
+        self.show_winner()    
 
     def show_instructions(self):
         # Display game instructions
         wait_time = 15
-        print(f'Waiting {wait_time} seconds for players to read instructions...')
-        emit("instructions", room=self.host_sid)
-        time.sleep(wait_time) # Wait for a bit so players can read the instructions
+        emit("instructions", {'wait': wait_time}, room=self.host_sid)
 
     def play_round(self, round_num):
         # Logic for each round
@@ -47,9 +52,7 @@ class EpigramGame:
         self.get_prompts(len(self.players))
         prompts = self.make_prompt_pairs()
         self.deliver_prompts(prompts)
-        self.collect_answers()
-        self.voting_phase()
-        self.show_results()
+        self.show_results(round_num)
         self.update_leaderboard()
 
     def get_prompts(self, numPlayers:int):
@@ -101,33 +104,76 @@ class EpigramGame:
             emit('new_prompt', {'prompt': self.players[i]['name']}, room=self.players[i]['sid'])
             emit('new_prompt', {'prompt': self.players[i]['name']}, room=self.players[i]['sid'])
 
-    def collect_answers(self, data):
-        # TODO: Need frontend solution to collect answers from players for prompts
+    def receive_answers(self, data):
+        player = self.find_player('sid', request.sid)
+        prompt = data.prompt
+        answer = data.answer
         pass
 
-    def voting_phase(self):
+    def receive_votes(self, data):
+        self.process_votes(self)
+        pass        
+
+    def process_votes(self, data):
         # Conduct voting for each prompt
+        player = self.find_player('sid', request.sid)
+        vote = data.vote # Can be player name
+        player_voted = self.find_player('name', vote)
+        player_voted['score'] += 50
         pass
 
-    def show_results(self):
+    def show_results(self, round_num):
         # Show results of voting
+        for prompt in self.prompt_answers[f'round {round_num}']:
+            print(prompt['prompt'])
         pass
 
     def update_leaderboard(self):
         # Update and display the leaderboard
+        # Sorting the list of dictionaries by 'score' in descending order
+        sorted_players = sorted(self.players, key=lambda x: x['score'], reverse=True)
+
+        # Extracting just the names in sorted order
+        sorted_names = [player['name'] for player in sorted_players]
+
+        emit('leaderboard', {'leaderboard_list': sorted_names}, room=self.host_sid)
+
+        print('Scores:')
+        for i, name in enumerate(sorted_names):
+            print(f'    {i}. {name}')
         pass
 
     def play_special_round(self):
         # Logic for the special round
-        print("Special Round")
+        print("--Special Round--")
         # Choose a special activity
         activity = random.choice(self.special_activities)
-        print(f"Special Activity: {activity}")
-        # Rest of the logic
+        activity()
+
+    def acro_lash(self):
+        print('Acro-Lash!')
+        emit('special_round')
+        pass
+
+    def comic_lash(self):
+        print('Comic-Lash!')
+        emit('special_round')
+        pass
+
+    def word_lash(self):
+        print('Word-Lash!')
+        emit('special_round')
+        pass
 
     def show_winner(self):
         # Determine and show the game winner
         pass
+
+    def find_player(self, category:str, match_value):
+        for player in self.players:
+            if player[category] == match_value:
+                return player
+        return None  # Return None if no player with the given sid is found
 
 if __name__ == "__main__":
     # To start the game
